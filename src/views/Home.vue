3e2e6fbe32cda8d297676db953d539a94afdd285
reiko_iguchi <reiko6122@gmail.com>
2022-02-10 20:29:31 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold my-3">
+          書籍検索
+        </h1>
+        <v-text-field 
+        label="著者名または作品名を入力してください。"
+        v-model="keyword"
+        required>
+        </v-text-field>
+        <v-btn @click="search"><span>検索</span></v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        keyword: ''
+      }
+    },
+    methods: {
+      search() {
+        this.$router.push(`/result/${this.keyword}`);
+      }
+    }
   }
-}
 </script>
---
+++ src/views/Result.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card
+    max-width="60%"
+    class="mx-auto"
+  >
+    <v-container>
+      <v-row dense>
+
+      <v-flex mb-4 text-center>
+        <h1 class="display-2 font-weight-bold my-3">
+          検索結果
+        </h1>
+        <p>
+          検索キーワード：{{ keyword }}
+        </p>
+      </v-flex>
+        <v-col
+          v-for="(book, i) in books"
+          :key="i"
+          cols="12"
+        >
+          <v-card
+            color="#777"
+            dark
+            :href="book.volumeInfo.infoLink"
+            target="_blank"
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="text-h5"
+                  v-text="book.volumeInfo.title"
+                ></v-card-title>
+                <div v-for="(author, j) in book.volumeInfo.authors" :key="j">
+                  <p class="px-4 py-1 mb-0">{{ author }}</p>
+                </div>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                tile
+              >
+                <v-img v-if="book.volumeInfo.imageLinks !== undefined && book.volumeInfo.imageLinks.smallThumbnail !== undefined" :src="book.volumeInfo.imageLinks.smallThumbnail  "></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios"
+  export default {
+    props: ['keyword'],
+    data() {
+      return {
+        books: [],
+      }
+    },
+    async created() {
+      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?maxResults=40&q=${this.keyword}`);
+      this.books = response.data.items;
+      console.log(this.books);
+    } 
+  }
+</script>
